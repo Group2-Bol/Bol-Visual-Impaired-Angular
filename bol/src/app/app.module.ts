c378bdf9119f67953d7bbb686bc2f154593e1a8b
GuidoGideon <guido.mehlkop@gmail.com>
2022-05-12 18:34:55 +0200

    Webscraper, image enhancement (deels): Webscraper zit in folder 'bol'. Je moet de path aanpassen die leid naar de chromedriver en daarnaast moet je selenium installeren zodat de scrpaer werkt (npm i selenium). Ik ga ervan uit dat er vier foto's worden ingeladen na het klikken op 'Bevestigen', dus hij pakt nu vier images en die enhanced. NOTE: de image-names zijn tijdelijk en ik ga het morgen verwerken in de modal.

--- bol/src/app/app.module.ts
+++ bol/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgEnhancementComponent } from './img-enhancement/img-enhancement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgEnhancementComponent
   ],
   imports: [
     BrowserModule,
