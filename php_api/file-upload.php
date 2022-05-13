926ad901648dcb00433f8b53bf152917163d1e69
FrostyDog132 <nielsnoordzij2001@gmail.com>
2022-05-13 09:54:58 +0200

    Changed image upload folder location and changed the uniqid name to ml5

--- php_api/file-upload.php
+++ php_api/file-upload.php
@@ -4,13 +4,13 @@
   header("Access-Control-Allow-Methods: PUT, GET, POST");
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-  $folderPath = "upload/";
+  $folderPath = "../bol/src/assets/images/";
 
   $file_temp = $_FILES['file']['tmp_name'];
   $tmp1 = explode('.', $_FILES['file']['name']);
   $tmp2 = end($tmp1);
   $file_ext = strtolower($tmp2);
-  $file = $folderPath . uniqid() . '.'.$file_ext;
+  $file = $folderPath . 'ml5' . '.'.$file_ext;
   move_uploaded_file($file_temp, $file);
   
 ?>
