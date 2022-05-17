67e596bce3b85106886f9cda8c116159ddea7e8c
GuidoGideon <guido.mehlkop@gmail.com>
2022-05-17 15:30:56 +0200

    Had eigenlijk eerst moeten mergen, dan pushen lmaoo

--- bol/src/app/overlay/overlay.component.ts
+++ bol/src/app/overlay/overlay.component.ts
@@ -95,4 +95,18 @@ export class OverlayComponent implements OnInit {
 				console.log(res);
 			})
 	}
+
+  loadImages() {
+    for (let i = 1; i <= 4; i++)
+    {
+      var imageTag = document.createElement('img');
+      console.log(`./../../assets/sample_images/bloem${i}`);
+      imageTag.src = `./../../assets/sample_images/bloem${i}.jpg`;
+      imageTag.style.maxHeight = '25%';
+      imageTag.style.maxWidth = '25%';
+      imageTag.style.filter = "contrast(2)";
+      imageTag.style.filter = "saturate(3)";
+      document.getElementById("images")?.appendChild(imageTag);
+    }
+  }
 }
