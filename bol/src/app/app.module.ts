a463a9317e19cdf0ec98ec37d45751ff182e1136
GuidoGideon <guido.mehlkop@gmail.com>
2022-05-25 14:08:47 +0200

    Laatste commit voor pull request

--- bol/src/app/app.module.ts
+++ bol/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgEnhancementComponent } from './img-enhancement/img-enhancement.component';
 import { ImageClassifierComponent } from './image-classifier/image-classifier.component';
 import { JstestComponent } from './jstest/jstest.component';
 import { OverlayComponent } from './overlay/overlay.component';
@@ -14,7 +13,6 @@ import { OverlayComponent } from './overlay/overlay.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ImgEnhancementComponent,
     ImageClassifierComponent,
     JstestComponent,
     OverlayComponent
